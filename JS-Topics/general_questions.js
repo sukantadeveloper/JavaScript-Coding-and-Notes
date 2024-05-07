6256d6cf706866dc4f0f42a1729c54a359d81275
Sukanta Ghosh <ghoshsu4@gmail.com>
2024-05-07 17:31:59 +0530

    general question added

---
+++ JS-Topics/general_questions.js
@@ -0,0 +1,14 @@
+// what is Function statement / declaration ?
+function a(){
+    console.log("Hello World");
+}
+// function expression \ difference between statement and expression 
+var b=function(){
+
+}
+
+// anonymous function 
+// named function expression 
+
+
+ // 
